ce06c22ad1b695992b5cbe0010616abb858e4bb2
DuberGarcia <duber.garcia@solucionessyh.com>
2025-08-01 09:58:29 -0500

    prueba erronea

--- test_playwright.py
+++ test_playwright.py
@@ -2,3 +2,10 @@ import re
 import pytest
 from playwright.async_api import async_playwright,expect
 
+@pytest.mark.asyncio
+async def test_api_success():
+    async with async_playwright() as p:
+        request_context = await p.request.new_context()
+        response = await request_context.get("https://jsonplaceholder.typicode.com/posts/1")
+        assert response.status == 200
+        await request_context.dis
