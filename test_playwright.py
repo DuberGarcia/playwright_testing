de44f7b11e22bdf307e68eb975cbe233416143e3
DuberGarcia <duber.garcia@solucionessyh.com>
2025-08-04 11:53:37 -0500

    test agregado

--- test_playwright.py
+++ test_playwright.py
@@ -1,4 +1,15 @@
-import re
-import pytest
-from playwright.async_api import async_playwright,expect
+import asyncio,pytest
+from playwright.async_api import Playwright, async_playwright, expect
 
+@pytest.mark.asyncio
+async def test_run():
+    async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch(headless=True)
+        context = await browser.new_context()
+        page = await context.new_page()
+        await page.goto("https://demoqa.com/automation-practice-form",wait_until="domcontentloaded")
+
+        # ---------------------
+        assert await page.title() == "DEMOQA"
+        await context.close()
+        await browser.close()
