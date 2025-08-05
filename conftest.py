508fa26ea2a46cfefc015846e7e45d29c798a5b0
DuberGarcia <duber.garcia@solucionessyh.com>
2025-08-05 10:16:51 -0500

    feat: fixture de page asyncronico

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from playwright.async_api import async_playwright
+
+@pytest.fixture
+async def page():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        context = await browser.new_context()
+        page = await context.new_page()
+        yield page
+        await context.close()
+        await browser.close()
--- test_playwright.py
+++ test_playwright.py
@@ -1,15 +1,7 @@
-import asyncio,pytest
-from playwright.async_api import Playwright, async_playwright, expect
+import pytest
 
 @pytest.mark.asyncio
-async def test_run():
-    async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch(headless=True)
-        context = await browser.new_context()
-        page = await context.new_page()
-        await page.goto("https://demoqa.com/automation-practice-form",wait_until="domcontentloaded")
+async def test_run(page):
+    await page.goto("https://demoqa.com/automation-practice-form")
+    assert await page.title() == "DEMOQA"
 
-        # ---------------------
-        assert await page.title() == "DEMOQA"
-        await context.close()
-        await browser.close()
